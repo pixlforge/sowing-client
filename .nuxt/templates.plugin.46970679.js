4427ac5d26d878b1c6e6322fba73f0a0c7196a1a
Pixlforge <celien@pixlforge.ch>
2018-10-19 22:19:09 +0200

    Built the project locally

---
+++ .nuxt/templates.plugin.46970679.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+config.autoAddCss = false
+
+
+  
+  
+    import  { far as fortawesomeproregularsvgicons_far } from '@fortawesome/pro-regular-svg-icons'
+    library.add(fortawesomeproregularsvgicons_far)
+  
+
+  
+  
+    import  { fas as fortawesomeprosolidsvgicons_fas } from '@fortawesome/pro-solid-svg-icons'
+    library.add(fortawesomeprosolidsvgicons_fas)
+  
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
