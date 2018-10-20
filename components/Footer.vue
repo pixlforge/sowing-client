8dcffa958f9d00669029f7c534ccc53909faf30f
Pixlforge <celien@pixlforge.ch>
2018-10-20 12:45:55 +0200

    Added basic disclaimer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,11 +2,18 @@
   <div>
     <div class="container">
       Footer
+      <Disclaimer/>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Disclaimer from "@/components/Disclaimer";
+
+export default {
+  components: {
+    Disclaimer
+  }
+};
 </script>
 
