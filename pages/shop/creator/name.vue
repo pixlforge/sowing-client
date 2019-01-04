a896601753c679efa9c75a862abf1915d084513f
Pixlforge <celien@pixlforge.ch>
2019-01-04 19:16:39 +0100

    Responsive styles for the shop creator terms page

--- pages/shop/creator/name.vue
+++ pages/shop/creator/name.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col items-center">
         <h2 class="title-large text-center mt-100">{{ $t("shop_creator.steps.name.title") }}</h2>
 
-        <section class="max-w-1000">
+        <section class="max-w-1000 px-20">
           <p class="paragraph-body text-center my-60">{{ $t("shop_creator.steps.name.paragraph") }}</p>
         </section>
 
