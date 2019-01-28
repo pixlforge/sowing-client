39f5cce8d8767f6bd1fbad6be363e4a46c9e9f6e
Pixlforge <celien@pixlforge.ch>
2019-01-28 11:40:25 +0100

    Refactored the shop terms to a component

--- pages/shop/creator/terms.vue
+++ pages/shop/creator/terms.vue
@@ -11,43 +11,14 @@
       </template>
     </Header>
 
+    <!-- Page contents -->
     <section class="container section">
+      
+      <!-- Terms -->
+      <AppTerms/>
+
+      <!-- Controls -->
       <div class="flex flex-col items-center">
-        <h2 class="title-large text-center mt-150">{{ $t("pages.terms.title") }}</h2>
-
-        <section class="max-w-1000">
-          
-          <h3 class="paragraph-title">Titre des conditions</h3>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim facere perspiciatis suscipit quod doloribus cumque, eum repellendus non consequuntur dolore quae dolorem perferendis explicabo minus, expedita tempore aperiam amet atque? Explicabo nobis id pariatur tempora accusamus totam voluptatibus facere nesciunt cum inventore, quibusdam sapiente! Error nisi earum perferendis et quod itaque tempora consequatur tempore, saepe veritatis laboriosam. Deleniti quibusdam beatae impedit tenetur aspernatur provident soluta itaque nulla inventore? Natus odio deleniti sunt omnis eveniet, porro iste debitis veritatis sint quis unde illum explicabo iusto aperiam corrupti eaque fugit facilis laboriosam nulla eligendi blanditiis? Vero fuga reiciendis officia deserunt obcaecati sapiente?
-          </p>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet incidunt eum, eligendi culpa quod rem consequatur? Minus modi ducimus molestias perspiciatis molestiae commodi, earum maxime exercitationem corrupti, magnam possimus omnis?
-          </p>
-          <h3 class="paragraph-title">Titre des conditions</h3>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim facere perspiciatis suscipit quod doloribus cumque, eum repellendus non consequuntur dolore quae dolorem perferendis explicabo minus, expedita tempore aperiam amet atque? Explicabo nobis id pariatur tempora accusamus totam voluptatibus facere nesciunt cum inventore, quibusdam sapiente! Error nisi earum perferendis et quod itaque tempora consequatur tempore, saepe veritatis laboriosam. Deleniti quibusdam beatae impedit tenetur aspernatur provident soluta itaque nulla inventore? Natus odio deleniti sunt omnis eveniet, porro iste debitis veritatis sint quis unde illum explicabo iusto aperiam corrupti eaque fugit facilis laboriosam nulla eligendi blanditiis? Vero fuga reiciendis officia deserunt obcaecati sapiente?
-          </p>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet incidunt eum, eligendi culpa quod rem consequatur? Minus modi ducimus molestias perspiciatis molestiae commodi, earum maxime exercitationem corrupti, magnam possimus omnis?
-          </p>
-          <h3 class="paragraph-title">Titre des conditions</h3>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim facere perspiciatis suscipit quod doloribus cumque, eum repellendus non consequuntur dolore quae dolorem perferendis explicabo minus, expedita tempore aperiam amet atque? Explicabo nobis id pariatur tempora accusamus totam voluptatibus facere nesciunt cum inventore, quibusdam sapiente! Error nisi earum perferendis et quod itaque tempora consequatur tempore, saepe veritatis laboriosam. Deleniti quibusdam beatae impedit tenetur aspernatur provident soluta itaque nulla inventore? Natus odio deleniti sunt omnis eveniet, porro iste debitis veritatis sint quis unde illum explicabo iusto aperiam corrupti eaque fugit facilis laboriosam nulla eligendi blanditiis? Vero fuga reiciendis officia deserunt obcaecati sapiente?
-          </p>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet incidunt eum, eligendi culpa quod rem consequatur? Minus modi ducimus molestias perspiciatis molestiae commodi, earum maxime exercitationem corrupti, magnam possimus omnis?
-          </p>
-          <h3 class="paragraph-title">Titre des conditions</h3>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim facere perspiciatis suscipit quod doloribus cumque, eum repellendus non consequuntur dolore quae dolorem perferendis explicabo minus, expedita tempore aperiam amet atque? Explicabo nobis id pariatur tempora accusamus totam voluptatibus facere nesciunt cum inventore, quibusdam sapiente! Error nisi earum perferendis et quod itaque tempora consequatur tempore, saepe veritatis laboriosam. Deleniti quibusdam beatae impedit tenetur aspernatur provident soluta itaque nulla inventore? Natus odio deleniti sunt omnis eveniet, porro iste debitis veritatis sint quis unde illum explicabo iusto aperiam corrupti eaque fugit facilis laboriosam nulla eligendi blanditiis? Vero fuga reiciendis officia deserunt obcaecati sapiente?
-          </p>
-          <p class="paragraph-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet incidunt eum, eligendi culpa quod rem consequatur? Minus modi ducimus molestias perspiciatis molestiae commodi, earum maxime exercitationem corrupti, magnam possimus omnis?
-          </p>
-
-        </section>
-
         <div class="text-18 text-center leading-normal mt-80">
           <label
             for="terms"
@@ -77,11 +48,13 @@
         </button>
       </div>
     </section>
+
   </main>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import AppTerms from "@/components/terms/AppTerms";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
@@ -92,7 +65,8 @@ export default {
     };
   },
   components: {
-    Header
+    Header,
+    AppTerms
   },
   computed: {
     ...mapGetters({
