926f786a74dcf013c99ad98655d2392b43632b6c
Pixlforge <celien@pixlforge.ch>
2019-03-11 17:34:12 +0100

    Refactored the header component classes

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-green">
-    <div class="container flex flex-col lg:flex-row items-center py-40">
+  <div class="header__background">
+    <div class="header__container container">
       <div>
-        <div class="w-110 h-110 rounded-full bg-white flex justify-center items-center text-48 text-green">
+        <div class="header__avatar">
           <slot name="icon"/>
         </div>
       </div>
-      <div class="text-center lg:text-left lg:ml-80 mt-40 lg:mt-0">
+      <div class="header__content">
         <slot name="title"/>
         <slot name="description"/>
       </div>
