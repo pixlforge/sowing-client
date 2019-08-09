b7bf0341d04c966ab8ffad22321d9b3e8c5d4d69
Pixlforge <celien@pixlforge.ch>
2019-08-09 18:59:24 +0200

    Add dropdown transitions

--- components/transitions/DropdownTransition.vue
+++ components/transitions/DropdownTransition.vue
@@ -1,7 +1,7 @@
 <template>
   <transition
-    enter-active-class="transition-all transition-faster ease-out"
-    leave-active-class="transition-all transition-medium ease-in"
+    enter-active-class="transition-all transition-fastest ease-out"
+    leave-active-class="transition-all transition-slow ease-in"
     enter-class="opacity-25 scale-75"
     enter-to-class="opacity-100 scale-100"
     leave-class="opacity-100 scale-100"
