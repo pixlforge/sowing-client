b170af40f3809f9f802a6e0bfeb85a0a08527e7e
Pixlforge <celien@pixlforge.ch>
2019-08-24 08:49:17 +0200

    Fix responsive styles

--- components/categories/AppCategoryBar.vue
+++ components/categories/AppCategoryBar.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="bg-green-200 py-5">
     <div class="container h-full flex items-center px-20">
-      <ul class="w-full flex flex-col md:flex-row flex-wrap justify-center xl:justify-between items-center -mx-10">
+      <ul class="w-full flex flex-wrap md:justify-center xl:justify-between -mx-10">
         <li
           v-for="category in categories"
           :key="category.slug"
+          class="w-1/2 md:w-auto"
         >
           <nuxt-link
             :to="localePath({ name: 'categories-slug', params: { slug: category.slug } })"
             :class="`hover:text-${shopTheme}-500`"
-            class="block no-underline text-16 xl:text-14 text-center transition-color transition-faster outline-none focus:shadow-outline rounded-lg px-30 xl:px-12 py-16 xl:py-10"
+            class="block h-full no-underline text-12 sm:text-14 text-center transition-color transition-faster outline-none focus:shadow-outline rounded-lg px-20 xl:px-12 py-12 xl:py-10 flex justify-center items-center"
           >
             {{ category.name[locale] }}
           </nuxt-link>
