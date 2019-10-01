067690182d8a00674725ca773808a256785d010d
Pixlforge <celien@pixlforge.ch>
2019-10-01 12:38:49 +0200

    Display current payment methods

--- pages/account/payment-methods/index.vue
+++ pages/account/payment-methods/index.vue
@@ -9,15 +9,37 @@
       {{ $t("pages.account.payment_methods.title") }}
     </AppTitle>
 
+    <!-- Payment methods -->
+    <div class="flex flex-wrap -mx-10 my-72 md:my-96">
+      <div
+        v-for="paymentMethod in paymentMethods"
+        :key="paymentMethod.id"
+        class="w-1/2 px-10"
+      >
+        <component
+          :is="paymentMethod.card_type_slug"
+          :payment-method="paymentMethod"
+          :selected-payment-method="selectedPaymentMethod.id === paymentMethod.id"
+          @click.native="switchPaymentMethod(paymentMethod)"
+        />
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import AppTitle from '@/components/AppTitle'
+import AppCardVisa from '@/components/payment-methods/cards/AppCardVisa'
+import AppCardAmex from '@/components/payment-methods/cards/AppCardAmex'
+import AppCardMasterCard from '@/components/payment-methods/cards/AppCardMasterCard'
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    visa: AppCardVisa,
+    'american-express': AppCardAmex,
+    mastercard: AppCardMasterCard
   },
   middleware: ['authenticated'],
   layout: 'account-management',
@@ -33,12 +55,32 @@ export default {
       ]
     }
   },
-  asyncData({ app }) {
-    // const user = await app.$axios.$get('/user/account')
+  data() {
+    return {
+      selectedPaymentMethod: {}
+    }
+  },
+  computed: {
+    defaultPaymentMethod() {
+      return this.paymentMethods.find(method => method.is_default)
+    }
+  },
+  async asyncData({ app }) {
+    const paymentMethods = await app.$axios.$get('/payment-methods')
 
     return {
-      // user: user.data,
+      paymentMethods: paymentMethods.data,
       title: app.head.title
+    }
+  },
+  created() {
+    if (this.paymentMethods.length) {
+      this.switchPaymentMethod(this.defaultPaymentMethod)
+    }
+  },
+  methods: {
+    switchPaymentMethod(paymentMethod) {
+      this.selectedPaymentMethod = paymentMethod
     }
   }
 }
