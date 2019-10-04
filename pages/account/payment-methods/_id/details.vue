bbf10fde13e88cac63daf0c31c232f6221d49b22
Pixlforge <celien@pixlforge.ch>
2019-10-04 18:30:01 +0200

    Page setup

--- pages/account/payment-methods/_id/details.vue
+++ pages/account/payment-methods/_id/details.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
-    Payment method details view
+
+    <!-- Page title -->
+    <AppTitle
+      semantic="h1"
+      visual="h1"
+    >
+      Title
+    </AppTitle>
+
   </div>
 </template>
 
 <script>
-export default {
+import AppTitle from '@/components/AppTitle'
 
+export default {
+  components: {
+    AppTitle
+  },
+  middleware: ['authenticated'],
+  layout: 'account-management',
+  head() {
+    return {
+      title: `${this.$t('pages.account.payment_methods.title')} | ${this.$t('pages.account.title')} | ${this.title}`,
+      meta: [
+        {
+          hid: 'robots',
+          name: 'robots',
+          content: 'noindex'
+        }
+      ]
+    }
+  },
+  asyncData({ app }) {
+    return {
+      title: app.head.title
+    }
+  }
 }
 </script>
