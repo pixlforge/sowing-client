af6abe7752318d1476216c4eab2b11db02237747
Pixlforge <celien@pixlforge.ch>
2019-10-05 12:31:16 +0200

    Basic view setup

---
+++ pages/account/payment-methods/_id/edit.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+
+    <!-- Page title -->
+    <AppTitle
+      semantic="h1"
+      visual="h1"
+    >
+      Edit
+    </AppTitle>
+
+  </div>
+</template>
+
+<script>
+import AppTitle from '@/components/AppTitle'
+
+export default {
+  components: {
+    AppTitle
+  },
+  middleware: ['authenticated'],
+  layout: 'account-management',
+  head() {
+    return {
+      title: `${this.$t('pages.account.payment_methods.title')} | ${this.$t('pages.account.title')} | ${this.title}`,
+      meta: [
+        {
+          hid: 'robots',
+          name: 'robots',
+          content: 'noindex'
+        }
+      ]
+    }
+  },
+  asyncData({ app }) {
+    return {
+      title: app.head.title
+    }
+  }
+}
+</script>
