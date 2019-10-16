75c6afbd1a6c24a2de94903e5a20fda594841051
Pixlforge <celien@pixlforge.ch>
2019-10-16 07:11:37 +0200

    Change route name

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,7 +1,7 @@
 export default function ({ app, redirect, route }) {
   if (!app.$auth.loggedIn) {
     return redirect({
-      name: 'login',
+      name: 'auth-login',
       query: {
         redirect: route.fullPath
       }
