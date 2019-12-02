19d618b48c83cedeb824125d12e72c26daf59285
Pixlforge <celien@pixlforge.ch>
2019-12-02 20:13:54 +0100

    Center layout horizontally and vertically

--- layouts/create-product.vue
+++ layouts/create-product.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
+  <div class="min-h-screen bg-gray-100 flex justify-center items-center">
     <AppContentSection>
       <nuxt/>
     </AppContentSection>
