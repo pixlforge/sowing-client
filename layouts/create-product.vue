1f55ec55f4ee641a8b43d48be8cf6306f0cc7889
Pixlforge <celien@pixlforge.ch>
2020-02-15 10:02:44 +0100

    Add Sowing logo

--- layouts/create-product.vue
+++ layouts/create-product.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="min-h-screen flex justify-center items-start bg-white">
+  <div class="min-h-screen flex flex-col bg-white pt-36">
     <main class="container py-36">
+      <LogoLink/>
       <Nuxt/>
     </main>
   </div>
 </template>
+
+<script>
+import LogoLink from '@/components/globals/LogoLink'
+
+export default {
+  components: {
+    LogoLink
+  }
+}
+</script>
