b707a9c981f23e2884a64dc66e4f259cbfc5d6b3
Pixlforge <celien@pixlforge.ch>
2020-02-16 17:19:31 +0100

    Add info bubble translations

--- lang/de-CH/productVariationType.js
+++ lang/de-CH/productVariationType.js
@@ -1,7 +1,8 @@
 export default {
   product_variation_type: {
     unnamed: 'Unbenannter Typ',
+    add_another_type: 'Fügen Sie einen weiteren Variationstyp hinzu',
     add_type: 'Fügen Sie Ihrem Produkt zunächst einen neuen Variationstyp hinzu',
-    add_another_type: 'Fügen Sie einen weiteren Variationstyp hinzu'
+    add_type_name_in_your_own_language: 'Sie sollten mindestens einen Namen in Ihrer Sprache hinzufügen.'
   }
 }
--- lang/it-IT/productVariationType.js
+++ lang/it-IT/productVariationType.js
@@ -1,7 +1,8 @@
 export default {
   product_variation_type: {
     unnamed: 'Tipo senza nome',
+    add_another_type: 'Aggiungere un altro tipo di variazione',
     add_type: 'Per prima cosa aggiungete un tipo di variazione al vostro prodotto',
-    add_another_type: 'Aggiungere un altro tipo di variazione'
+    add_type_name_in_your_own_language: 'Come minimo, dovreste aggiungere un nome nella vostra lingua.'
   }
 }
