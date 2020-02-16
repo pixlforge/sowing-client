41b49912b800a11c98c335f6fa2a4525c841301d
Pixlforge <celien@pixlforge.ch>
2020-02-16 18:32:55 +0100

    Fix bugged transitions

--- components/transitions/DropdownTransition.vue
+++ components/transitions/DropdownTransition.vue
@@ -1,11 +1,11 @@
 <template>
   <transition
-    enter-active-class="transition-all transition-fastest ease-out"
-    leave-active-class="transition-all transition-slow ease-in"
-    enter-class="opacity-25 scale-75"
-    enter-to-class="opacity-100 scale-100"
-    leave-class="opacity-100 scale-100"
-    leave-to-class="opacity-25 scale-75"
+    enter-active-class="transition-all duration-100 ease-out"
+    leave-active-class="transition-all duration-200 ease-in"
+    enter-class="origin-top transform opacity-0 scale-y-0"
+    enter-to-class="opacity-100 scale-y-100"
+    leave-class="transform opacity-100"
+    leave-to-class="transform opacity-0"
   >
     <slot/>
   </transition>
--- components/transitions/ModalTransition.vue
+++ components/transitions/ModalTransition.vue
@@ -1,11 +1,11 @@
 <template>
   <transition
-    enter-active-class="transition-opacity transition-faster ease-out"
-    leave-active-class="transition-opacity transition-faster ease-in"
-    enter-class="opacity-25"
+    enter-active-class="transition-opacity duration-200 ease-out"
+    leave-active-class="transition-opacity duration-200 ease-in"
+    enter-class="opacity-0"
     enter-to-class="opacity-100"
     leave-class="opacity-100"
-    leave-to-class="opacity-25"
+    leave-to-class="opacity-0"
   >
     <slot/>
   </transition>
