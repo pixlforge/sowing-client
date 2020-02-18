0ba6c70e984690104fb5fe9ade60c3a18274b207
Pixlforge <celien@pixlforge.ch>
2020-02-18 18:43:41 +0100

    Add animation

--- components/transitions/ModalTransition.vue
+++ components/transitions/ModalTransition.vue
@@ -1,7 +1,7 @@
 <template>
   <transition
-    enter-active-class="transition-opacity duration-200 ease-out"
-    leave-active-class="transition-opacity duration-200 ease-out"
+    enter-active-class="transition-opacity duration-100 ease-out"
+    leave-active-class="transition-opacity duration-100 ease-in"
     enter-class="opacity-0"
     enter-to-class="opacity-100"
     leave-class="opacity-100"
